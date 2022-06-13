327a59e4140b5a72f6a5d2382cc52465852fef8f
SergioRuyDev <sergioruyenator@gmail.com>
2022-06-13 17:13:04 -0300

    Study concepts about entities.

---
+++ src/application/usecases/create-challenge-submission.ts
@@ -0,0 +1,17 @@
+import {Submission} from "../../domain/entities/submission";
+
+type CreateChallengeSubmissionRequest = {
+    userId: string;
+    challengeId: string
+}
+
+class CreateChallengeSubmission {
+    execute({ userId, challengeId }: CreateChallengeSubmissionRequest) {
+        const submission = Submission.create({
+            studentId,
+            challengeId,
+        })
+
+        return submission
+    }
+}
